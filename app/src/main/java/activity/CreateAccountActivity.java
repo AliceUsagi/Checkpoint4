b292d70e7f7b137a847228568a511d1363a63099
Joséphine Godard <josephine.godard@gmail.com>
2019-07-16 13:41:44 +0200

    Appel des boutons et textView  dans create + condition de remplir tous les champs.

--- app/src/main/java/activity/CreateAccountActivity.java
+++ app/src/main/java/activity/CreateAccountActivity.java
@@ -1,7 +1,13 @@
 package activity;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.livres.R;
 
@@ -11,5 +17,34 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        TextView tvAlreadyUser = findViewById(R.id.tvAlreadyUser);
+        tvAlreadyUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateAccountActivity.this, signInActivity.class));
+            }
+        });
+
+        Button btCreate = findViewById(R.id.btCreate);
+        btCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etPseudo = findViewById(R.id.etPseudo);
+                EditText etEmail = findViewById(R.id.etMail);
+                EditText etPw = findViewById(R.id.etPw);
+                String pseudo = etPseudo.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPw.getText().toString();
+                if (pseudo.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    new AlertDialog.Builder(CreateAccountActivity.this)
+                            .setTitle("Error")
+                            .setMessage(R.string.veuillez_remplir_les_champs_pour_continuer)
+                            .show();
+                    return;
+                }
+                startActivity(new Intent(CreateAccountActivity.this, signInActivity.class));
+            }
+        });
     }
 }
