75e5e332c81f0b701623436fcf943bc4c4b64bf2
Joséphine Godard <josephine.godard@gmail.com>
2019-07-16 14:01:47 +0200

    Ajout de la condition de remplir les champs sur la page SignIn

--- app/src/main/java/activity/signInActivity.java
+++ app/src/main/java/activity/signInActivity.java
@@ -1,7 +1,11 @@
 package activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.livres.R;
 
@@ -11,5 +15,24 @@ public class signInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button btCreate = findViewById(R.id.btSendSignIn);
+        btCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etEmailSignIn = findViewById(R.id.etMailSignIn);
+                EditText etPwSignIn = findViewById(R.id.etPwSignIn);
+                String email = etEmailSignIn.getText().toString();
+                String password = etPwSignIn.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    new AlertDialog.Builder(signInActivity.this)
+                            .setTitle("Error")
+                            .setMessage(R.string.veuillez_remplir_les_champs_pour_continuer)
+                            .show();
+                    return;
+                }
+                //startActivity(new Intent(signInActivity.this, signInActivity.class));
+            }
+        });
     }
 }
